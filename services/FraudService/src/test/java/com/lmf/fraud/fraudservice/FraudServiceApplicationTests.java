07dab1afcfb370d5b4d7e3ed446369ddb01c8f2d
LeandroMF <leozztto@gmail.com>
2026-05-17 00:03:57 -0300

    ajuste na nomenclatura dos services

---
+++ services/FraudService/src/test/java/com/lmf/fraud/fraudservice/FraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmf.fraud.fraudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/InventoryService/src/main/java/com/lmf/inventory/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lmf.inventory.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}

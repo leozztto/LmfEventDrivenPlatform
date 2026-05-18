68b16a2b5118606373bbf2266c3bfcdf037bd019
LeandroMF <leozztto@gmail.com>
2026-05-17 22:55:25 -0300

    ajuste no mapper para retornar objeto correto

--- services/OrderService/src/main/java/com/lmf/order/orderservice/infrastructure/persistence/mapper/OrderEntityMapper.java
+++ services/OrderService/src/main/java/com/lmf/order/orderservice/infrastructure/persistence/mapper/OrderEntityMapper.java
@@ -4,53 +4,33 @@ import com.lmf.order.orderservice.domain.model.Order;
 import com.lmf.order.orderservice.domain.model.OrderItem;
 import com.lmf.order.orderservice.infrastructure.persistence.entity.OrderEntity;
 import com.lmf.order.orderservice.infrastructure.persistence.entity.OrderItemEntity;
+import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
 
+import java.util.UUID;
+
+@Component
 public class OrderEntityMapper {
 
     public OrderEntity toEntity(Order order) {
 
-        OrderEntity entity = new OrderEntity(
-                order.getId(),
-                order.getCustomerId(),
-                order.getOrderStatus(),
-                order.getTotalAmount(),
-                order.getCreatedAt()
-        );
+        OrderEntity entity = new OrderEntity(order.getId(), order.getCustomerId(), order.getOrderStatus(), order.getTotalAmount(), order.getCreatedAt());
 
-        order.getOrderItems()
-                .stream()
-                .map(this::toItemEntity)
-                .forEach(entity::addItem);
+        order.getOrderItems().stream().map(this::toItemEntity).forEach(entity::addItem);
 
         return entity;
     }
 
     private OrderItemEntity toItemEntity(OrderItem item) {
 
-        return new OrderItemEntity(
-                java.util.UUID.randomUUID(),
-                item.getProductId(),
-                item.getQuantity(),
-                item.getUnitPrice(),
-                item.getSubtotal()
-        );
+        return new OrderItemEntity(UUID.randomUUID(), item.getProductId(), item.getQuantity(), item.getUnitPrice(), item.getSubtotal());
     }
 
-    public Order toDomain(OrderEntity entity) {
+    public Order toDomain(OrderEntity orderEntity) {
 
-        return new Order(
-                entity.getCustomerId(),
+        var orderItems = orderEntity.getOrderItemsEntities().stream().map(item -> new OrderItem(item.getProductId(), item.getQuantity(), item.getUnitPrice())).toList();
 
-                entity.getOrderItemsEntities()
-                        .stream()
-                        .map(item -> new OrderItem(
-                                item.getProductId(),
-                                item.getQuantity(),
-                                item.getUnitPrice()
-                        ))
-                        .collect(Collectors.toList())
-        );
+        return new Order(orderEntity.getId(), orderEntity.getCustomerId(), orderItems, orderEntity.getOrderStatus(), orderEntity.getCreatedAt());
     }
 }

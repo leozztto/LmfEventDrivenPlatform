2804e14a6c2049205c72d88d8494ce2cd3c5f751
LeandroMF <leozztto@gmail.com>
2026-05-23 10:40:01 -0300

    ajustado enum

--- services/PaymentService/src/main/java/com/lmf/payment/paymentservice/domain/PaymentMethod.java
+++ services/PaymentService/src/main/java/com/lmf/payment/paymentservice/domain/PaymentMethod.java
@@ -1,6 +1,7 @@
 package com.lmf.payment.paymentservice.domain;
 
 public enum PaymentMethod {
+
     CREDIT_CARD, DEBIT_CARD, PIX, BOLETO, PAYPAL, APPLE_PAY, GOOGLE_PAY;
 
     public static PaymentMethod fromName(String name) {

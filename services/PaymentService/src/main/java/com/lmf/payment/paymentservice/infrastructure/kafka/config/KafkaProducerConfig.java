8a9db87a1c944701766146ddf5e8536171de34f6
LeandroMF <leozztto@gmail.com>
2026-05-24 16:52:46 -0300

    ajustados configuracoes kafka

--- services/PaymentService/src/main/java/com/lmf/payment/paymentservice/infrastructure/kafka/config/KafkaProducerConfig.java
+++ services/PaymentService/src/main/java/com/lmf/payment/paymentservice/infrastructure/kafka/config/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +22,7 @@ public class KafkaProducerConfig {
 
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(config);
     }
